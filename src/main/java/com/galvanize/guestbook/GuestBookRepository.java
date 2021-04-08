4814f446c8954acab04a2caba5f46f54fd6f705f
David Grieser <david.grieser@galvanize.com>
2021-04-08 11:14:29 -0500

    RED - Unit Test - Create classes
    failing on saveEntry for service

---
+++ src/main/java/com/galvanize/guestbook/GuestBookRepository.java
@@ -0,0 +1,4 @@
+package com.galvanize.guestbook;
+
+public class GuestBookRepository {
+}
---
+++ src/main/java/com/galvanize/guestbook/GuestBookService.java
@@ -0,0 +1,6 @@
+package com.galvanize.guestbook;
+
+import org.springframework.stereotype.Service;
+
+public class GuestBookService {
+}
