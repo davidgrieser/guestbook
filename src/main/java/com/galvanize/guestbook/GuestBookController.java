a8152849dce49576fc3f834e36775dca48ac3957
David Grieser <david.grieser@galvanize.com>
2021-04-08 11:06:59 -0500

    RED - Many - Write implementation desired for service

--- src/main/java/com/galvanize/guestbook/GuestBookController.java
+++ src/main/java/com/galvanize/guestbook/GuestBookController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class GuestBookController {
 
+    GuestBookService guestBookService;
+
+    GuestBookController(GuestBookService guestBookService) {
+        this.guestBookService = guestBookService;
+    }
+
     @GetMapping("entries")
     public String getEntries() {
         return "[]";
@@ -14,6 +20,7 @@ public class GuestBookController {
     @PostMapping("entries")
     @ResponseStatus(HttpStatus.CREATED)
     public EntryDto postEntries(@RequestBody EntryDto entry) {
+        guestBookService.saveEntry(entry);
         return entry;
     }
 
