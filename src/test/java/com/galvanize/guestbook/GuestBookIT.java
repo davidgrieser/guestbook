5d1e2a1111380679392183a32d305c71f4420f40
David Grieser <david.grieser@galvanize.com>
2021-04-08 10:52:36 -0500

    RED - One entry being posted - fails with 405

--- src/test/java/com/galvanize/guestbook/GuestBookIT.java
+++ src/test/java/com/galvanize/guestbook/GuestBookIT.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,5 +24,15 @@ public class GuestBookIT {
         mockMvc.perform(get("/entries"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(0));
+    }
+
+    @Test
+    void postEntry() throws Exception {
+        mockMvc.perform(post("/entries")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\": \"David\", \"comment\":\"Making sure this works\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("name").value("David"))
+                .andExpect(jsonPath("comment").value("Making sure this works"));
     }
 }
