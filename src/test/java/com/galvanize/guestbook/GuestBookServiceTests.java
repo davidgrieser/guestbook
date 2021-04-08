295c99d810829abd132decb7888752a78f4d083c
David Grieser <david.grieser@galvanize.com>
2021-04-08 11:34:52 -0500

    RED - Unit Test - getEntries method missing

--- src/test/java/com/galvanize/guestbook/GuestBookServiceTests.java
+++ src/test/java/com/galvanize/guestbook/GuestBookServiceTests.java
@@ -6,7 +6,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GuestBookServiceTests {
@@ -24,5 +28,25 @@ public class GuestBookServiceTests {
         verify(mockGuestBookRepository).save(
                 new EntryEntity("Name1", "Comment1")
         );
+    }
+
+    @Test
+    void getEntries() {
+        EntryEntity firstEntryEntity = new EntryEntity("GetName1", "GetComment1");
+        EntryEntity secondEntryEntity = new EntryEntity("GetName2", "GetComment2");
+
+        when(mockGuestBookRepository.findAll()).thenReturn(
+                List.of(firstEntryEntity, secondEntryEntity)
+        );
+
+        List<EntryDto> actual = service.getEntries();
+
+        assertThat(actual).isEqualTo(
+                List.of(
+                         new EntryDto("GetName1", "GetComment1"),
+                         new EntryDto("GetName2", "GetComment2")
+                )
+        );
+
     }
 }
